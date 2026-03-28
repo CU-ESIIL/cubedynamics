ef7b3cb8b65802330724383efd6ed61307d92778
Ty Tuff <ty.tuff@colorado.edu>
2026-03-28 16:33:14 -0600

    test: remove redundant import/streaming stub smoke checks

--- src/cubedynamics/tests/test_imports.py
+++ src/cubedynamics/tests/test_imports.py
@@ -1,7 +1,5 @@
 """Basic import smoke tests for the cubedynamics package."""
 
-import pytest
-
 import cubedynamics
 
 
@@ -19,17 +17,3 @@ def test_streaming_helpers_are_callable():
     ]
     for func in public_funcs:
         assert callable(func)
-
-
-def test_streaming_stubs_raise_not_implemented():
-    """Only the unimplemented helpers should raise NotImplementedError."""
-    stub_funcs = [
-        cubedynamics.stream_prism_to_cube,
-        cubedynamics.correlation_cube,
-    ]
-    for func in stub_funcs:
-        with pytest.raises(NotImplementedError):
-            if func is cubedynamics.correlation_cube:
-                func(None, [None])
-            else:
-                func(None)
